7f196be63722f741943c5cf84086340144faf2cf
Mike Zukowsky <michal@durooil.com>
2015-09-09 15:24:33 +0200

    Update CurlProvider.php

--- src/Provider/CurlProvider.php
+++ src/Provider/CurlProvider.php
@@ -3,9 +3,16 @@ namespace mxreCaptcha\Provider;
 
 class CurlProvider implements ProviderInterface
 {
+	const URL = 'https://www.google.com/recaptcha/api/siteverify';
+	
+	public function __construct($url = self::URL)
+	{
+		$this->url = $url;
+	}
+	
     public function isResponseValid($secret, $response, $ip)
     {
-        $ch = curl_init('https://www.google.com/recaptcha/api/siteverify');
+        $ch = curl_init($this->url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
